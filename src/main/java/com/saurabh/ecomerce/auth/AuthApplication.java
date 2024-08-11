b52970dca76bfbd10bbff7af675a2ffe6eb4054e
saurabh <guptasaurabh5858@gmail.com>
2024-08-11 18:11:00 +0530

    first commit spring boot

---
+++ src/main/java/com/saurabh/ecomerce/auth/AuthApplication.java
@@ -0,0 +1,15 @@
+package com.saurabh.ecomerce.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApplication {
+
+	public static void main(String[] args) {
+		//System.out.printf("saurabh");
+		System.out.println("Hello, Gradle Spring Project!");
+		SpringApplication.run(AuthApplication.class, args);
+	}
+
+}
